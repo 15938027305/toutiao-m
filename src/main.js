c1fc3eb3ad90295b7f0cf44b975c4ac1cf5c57d6
jiaochao <1262028356@qq.com>
2022-11-15 15:09:59 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+// 项目入口文件
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './styles/index.less'
 Vue.config.productionTip = false
 
 new Vue({
